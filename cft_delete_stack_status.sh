7797082c1c6387bac3c63e7f797cd2b413a71262
root <abrahamdimma@gmail.com>
2020-04-11 17:30:48 +0000

    update commit

--- cft_delete_stack_status.sh
+++ cft_delete_stack_status.sh
@@ -1,10 +1,10 @@
 if [[ $? -eq 0 ]];
    then
    echo "Openshift cluster stack deletion is in progress"
-   delete_stack_status=$(aws --region $1 cloudformation delete-stack --stack-name $2 --query 'Stacks[0].StackStatus' --output text)
+   delete_stack_status=$(aws --region $1 cloudformation wait stack-delete-complete --stack-name $2 --query 'Stacks[0].StackStatus' --output text)
    while [[ $delete_stack_status == "REVIEW_IN_PROGRESS" ]] || [[ $delete_stack_status == "$3" ]]
    do
    sleep 30
-   delete_stack_status=$(aws --region $1 cloudformation delete-stack --stack-name $2 --query 'Stacks[0].StackStatus' --output text)
+   delete_stack_status=$(aws --region $1 cloudformation wait stack-delete-complete --stack-name $2 --query 'Stacks[0].StackStatus' --output text)
    done
 fi
